696735cedec98bc160e4f428e81ae3fe2fd627c2
Roman Koch <koch.roman@gmail.com>
2025-03-16 12:48:13 +0100

    fixed workflow

--- .devcontainer/rebuild-image.sh
+++ .devcontainer/rebuild-image.sh
@@ -4,8 +4,24 @@
 # SPDX-Author: Roman Koch <koch.romam@gmail.com>
 # SPDX-Copyright: 2025 Roman Koch <koch.romam@gmail.com>
 
-docker build -t ghcr.io/emblincram/boxy:latest .
+set -euo pipefail
 
-echo "${CR_PAT}" | docker login ghcr.io -u emblincram --password-stdin
-docker push ghcr.io/emblincram/boxy:latest
+DATE_TAG=$(date +%Y%m%d-%H%M)
+ORGANISATION="emblincram"
+CONTAINER_NAME=basename $(git rev-parse --show-toplevel)
 
+if [[ -z "${GITHUB_TOKEN:-}" ]]; then
+    echo "error: GITHUB_TOKEN is not set." >&2
+    exit 1
+fi
+
+CR_PAT=$GITHUB_TOKEN
+
+docker build \
+        -t ghcr.io/$ORGANISATION/$CONTAINER_NAME:latest \
+        -t ghcr.io/$ORGANISATION/$CONTAINER_NAME:$DATE_TAG \
+        -f Dockerfile .
+
+echo "${CR_PAT}" | docker login ghcr.io -u "${ORGANISATION}" --password-stdin
+docker push ghcr.io/$ORGANISATION/$CONTAINER_NAME:latest
+docker push ghcr.io/$ORGANISATION/$CONTAINER_NAME:$DATE_TAG
